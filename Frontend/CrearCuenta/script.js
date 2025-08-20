7dfce7e8047fe9cf212cc46edbd4ffcb5c6dd811
Lucas Schniper <you@example.com>
2025-08-20 15:56:55 -0300

    avance con los id

---
+++ Frontend/CrearCuenta/script.js
@@ -0,0 +1,8 @@
+let NombreDeUsuario = document.getElementById ("button-entrar")
+let Mail = document.getElementById ("mail")
+let contraseña = document.getElementById ("Contraseña")
+let FechaDeNacimiento = document.getElementById ("calendario")
+let Terminos = document.getElementById ("Terminos")
+
+
+
